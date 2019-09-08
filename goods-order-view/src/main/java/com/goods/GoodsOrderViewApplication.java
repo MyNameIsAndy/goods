883e20398ae7d8c9259abecb3923c4e5e3840fdd
andy <3235109437@qq.com>
2019-09-08 21:24:24 +0800

    总结

---
+++ goods-order-view/src/main/java/com/goods/GoodsOrderViewApplication.java
@@ -0,0 +1,13 @@
+package com.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsOrderViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsOrderViewApplication.class, args);
+	}
+
+}
