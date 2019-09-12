595122423538b2ffcb410659b53c892a2fce3586
Andy <yunchao.yue@bj.shinsoft.net>
2019-09-12 17:51:21 +0800

    配置文件

--- goods-order-view/src/main/java/com/goods/GoodsOrderViewApplication.java
+++ goods-order-view/src/main/java/com/goods/GoodsOrderViewApplication.java
@@ -1,13 +1,22 @@
 package com.goods;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class GoodsOrderViewApplication {
-
+public class GoodsOrderViewApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+	private static Logger logger = LoggerFactory.getLogger(GoodsOrderViewApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(GoodsOrderViewApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GoodsOrderViewApplication.class);
 	}
 
 }
--- goods-order-view/src/main/java/com/goods/controller/ControllerTest.java
+++ goods-order-view/src/main/java/com/goods/controller/ControllerTest.java
@@ -1,12 +1,18 @@
 package com.goods.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControllerTest {
+    @Value("${b}")
+    private String b;
     @RequestMapping("/index")
-    public String test(){
-        return "index";
+    public ModelAndView test(){
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
     }
 }
