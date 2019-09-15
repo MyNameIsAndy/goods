70dc35d5f1c66f4537ef04402ec1a6ba56150993
andy <3235109437@qq.com>
2019-09-15 22:27:05 +0800

    总结

---
+++ goods-manager/goods-manager-mapper/src/main/java/com/goods/baseDao/BaseUpdateMapper.java
@@ -0,0 +1,16 @@
+package com.goods.baseDao;
+
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.UpdateProvider;
+
+public interface BaseUpdateMapper<T> {
+    /**
+     * 根据主键更新属性不为null的值
+     *
+     * @param record
+     * @return
+     */
+    @UpdateProvider(type = BaseUpdateProvider.class, method = "dynamicSQL")
+    @Options(useCache = false, useGeneratedKeys = false)
+    int updateEmptyBySelective(T record);
+}
---
+++ goods-manager/goods-manager-mapper/src/main/java/com/goods/baseDao/BaseUpdateProvider.java
@@ -0,0 +1,26 @@
+package com.goods.baseDao;
+
+import org.apache.ibatis.mapping.MappedStatement;
+import tk.mybatis.mapper.mapperhelper.MapperHelper;
+import tk.mybatis.mapper.mapperhelper.MapperTemplate;
+import tk.mybatis.mapper.mapperhelper.SqlHelper;
+
+public class BaseUpdateProvider extends MapperTemplate {
+    public BaseUpdateProvider(Class<?> mapperClass, MapperHelper mapperHelper) {
+        super(mapperClass, mapperHelper);
+    }
+    /**
+     * 通过主键更新不为null的字段
+     *
+     * @param ms
+     * @return
+     */
+    public String updateEmptyBySelective(MappedStatement ms) {
+        Class<?> entityClass = getEntityClass(ms);
+        StringBuilder sql = new StringBuilder();
+        sql.append(SqlHelper.updateTable(entityClass, tableName(entityClass)));
+        sql.append(SqlHelper.updateSetColumns(entityClass, null, true, false));
+        sql.append(SqlHelper.wherePKColumns(entityClass));
+        return sql.toString();
+    }
+}
