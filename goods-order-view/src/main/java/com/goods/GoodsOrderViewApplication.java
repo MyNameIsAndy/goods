a71c7f03cf58edb01ab31e18eb00102746caea84
andy <3235109437@qq.com>
2019-09-15 22:31:04 +0800

    总结

--- goods-order-view/src/main/java/com/goods/GoodsOrderViewApplication.java
+++ goods-order-view/src/main/java/com/goods/GoodsOrderViewApplication.java
@@ -1,14 +1,17 @@
 package com.goods;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
+@MapperScan("com.goods.mapper")
 public class GoodsOrderViewApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 	private static Logger logger = LoggerFactory.getLogger(GoodsOrderViewApplication.class);
 	public static void main(String[] args) {
