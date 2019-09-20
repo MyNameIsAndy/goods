7cbd27e2f572b2d73c52212931c273d331467bce
Andy <yunchao.yue@bj.shinsoft.net>
2019-09-20 21:37:26 +0800

    删除冗余

--- goods-order/src/main/java/com/goods/mapper/ow/OwUserMapper.java
+++ goods-order/src/main/java/com/goods/mapper/ow/OwUserMapper.java
@@ -1,7 +1,7 @@
 package com.goods.mapper.ow;
 
 import com.goods.baseDao.BaseMapper;
-import com.goods.common.OwUser;
+import com.goods.common.ow.OwUser;
 
 public interface OwUserMapper extends BaseMapper<OwUser> {
 }
