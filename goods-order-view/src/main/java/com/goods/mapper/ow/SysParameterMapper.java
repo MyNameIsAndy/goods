a8a9a96f5fc0064d48b94238c17cde767e1edf57
Andy <yunchao.yue@bj.shinsoft.net>
2019-09-20 21:44:38 +0800

    删除冗余

--- goods-order-view/src/main/java/com/goods/mapper/ow/SysParameterMapper.java
+++ goods-order-view/src/main/java/com/goods/mapper/ow/SysParameterMapper.java
@@ -1,7 +1,7 @@
 package com.goods.mapper.ow;
 
 import com.goods.baseDao.BaseMapper;
-import com.goods.common.SysParameter;
+import com.goods.common.sys.SysParameter;
 
 /**
  * @Classname SysParameter
