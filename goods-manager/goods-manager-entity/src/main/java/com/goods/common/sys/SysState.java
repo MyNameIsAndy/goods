5e430b75f52326a88ec21b0f47cff6b1959fe05e
Andy <yunchao.yue@bj.shinsoft.net>
2019-09-20 21:40:17 +0800

    删除冗余

--- goods-manager/goods-manager-entity/src/main/java/com/goods/common/sys/SysState.java
+++ goods-manager/goods-manager-entity/src/main/java/com/goods/common/sys/SysState.java
@@ -2,13 +2,14 @@ package com.goods.common.sys;
 
 import com.goods.base.BaseBean;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Table;
 
 @Table(name = "sys_state")
-@Getter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SysState extends BaseBean{
