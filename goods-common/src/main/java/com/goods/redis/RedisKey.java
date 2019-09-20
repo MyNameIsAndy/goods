d8a4931bd12b4bf960120a4eda6b1c5854b98fba
Andy <yunchao.yue@bj.shinsoft.net>
2019-09-20 21:39:10 +0800

    删除冗余

--- goods-common/src/main/java/com/goods/redis/RedisKey.java
+++ goods-common/src/main/java/com/goods/redis/RedisKey.java
@@ -8,4 +8,12 @@ public class RedisKey {
      * 所有商品的key 缓存类型map
      */
     public static final String GOODS_REDIS_ORDER_MAP="goods_redis_order_map";
+    /**
+     * 所有系统参数
+     */
+    public static final String GOODS_SYS_PARAMETER="goods_sys_parameter";
+    /**
+     * 系统状态码
+     */
+    public static final String GOODS_SYS_STATE="goods_sys_state";
 }
