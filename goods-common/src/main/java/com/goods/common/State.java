88fc5d0d1a5018f73caf7e1acdc7bf1dde3fb9b1
andy <3235109437@qq.com>
2019-09-22 22:10:53 +0800

    总结

--- goods-common/src/main/java/com/goods/common/State.java
+++ goods-common/src/main/java/com/goods/common/State.java
@@ -5,12 +5,39 @@ import lombok.Getter;
 @Getter
 public enum  State {
     /**
+     * goods-order服务模块
+     */
+    GOODS_SERVICE_ORDER("P0001"),
+    /**
+     * goods-order-view服务模块
+     */
+    GOODS_SERVICE_ORDER_VIEW("P0002"),
+
+
+
+
+    /**
      * 成功
      */
     SUCCESS_CODE("10000"),
 
 
 
+    /**
+     *处理中
+     */
+    DOING("21002"),
+
+
+
+    /**
+     * 失败
+     */
+    ERROR_CODE("30000"),
+    /**
+     * 业务数据不合规
+     */
+    BUSINESS_DATA_INVALID("31000"),
     ;
 
     private String code;
--- goods-common/src/main/java/com/goods/redis/RedisKey.java
+++ goods-common/src/main/java/com/goods/redis/RedisKey.java
@@ -7,7 +7,7 @@ public class RedisKey {
     /**
      * 所有商品的key 缓存类型map
      */
-    public static final String GOODS_REDIS_ORDER_MAP="goods_redis_order_map";
+    public static final String GOODS_ORDER_MAP="goods_order_map";
     /**
      * 所有系统参数
      */
@@ -16,4 +16,8 @@ public class RedisKey {
      * 系统状态码
      */
     public static final String GOODS_SYS_STATE="goods_sys_state";
+    /**
+     * 所有服务模块
+     */
+    public static final String GOODS_SYS_SEVICE_MODEL="goods_sys_service_model";
 }
