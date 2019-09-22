5b2ebe673125fc8c949e966748a3451d4517e22b
andy <3235109437@qq.com>
2019-09-22 22:11:43 +0800

    总结

---
+++ goods-order/src/main/java/com/goods/mapper/sys/SysSerParameterMepper.java
@@ -0,0 +1,13 @@
+package com.goods.mapper.sys;
+
+import com.goods.baseDao.BaseMapper;
+import com.goods.common.sys.SysSerParameter;
+
+/**
+ * @Classname SysCmdParameter
+ * @Description TODO
+ * @Date 2019/9/21 15:04
+ * @Created by andy
+ */
+public interface SysSerParameterMepper extends BaseMapper<SysSerParameter>{
+}
