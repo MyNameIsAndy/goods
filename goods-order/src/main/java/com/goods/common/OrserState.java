24f3d7870e28ae7286fabee12f91c47dd3f96db4
andy <3235109437@qq.com>
2019-09-22 22:11:17 +0800

    总结

---
+++ goods-order/src/main/java/com/goods/common/OrserState.java
@@ -0,0 +1,14 @@
+package com.goods.common;
+
+import lombok.Getter;
+
+/**
+ * @Classname OrserState
+ * @Description TODO
+ * @Date 2019/9/21 20:38
+ * @Created by andy
+ */
+@Getter
+public enum OrserState {
+
+}
